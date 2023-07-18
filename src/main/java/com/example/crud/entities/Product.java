2867b5b9e419c4bdc35fb9197e311187539caa13
João Victor Correa <correa.joaov25@gmail.com>
2023-07-18 13:31:21 -0300

    Product Controller, Entity Product and configs with application.properties

---
+++ src/main/java/com/example/crud/entities/Product.java
@@ -0,0 +1,74 @@
+package com.example.crud.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private double price;
+	
+	public Product() {}
+
+	public Product(Long id, @NotNull String name, @NotNull double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
